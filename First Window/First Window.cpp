3e38c556e8dad0b3a1da9db4f208c72262d5e85b
Sam Mathis <smmathis97@gmail.com>
2022-11-09 23:12:53 -0700

    Add project files.

---
+++ First Window/First Window.cpp
@@ -0,0 +1,18 @@
+#include <SDL.h>
+
+int main(int argc, char* args[]) {
+
+	SDL_Init(SDL_INIT_EVERYTHING);
+	SDL_Window* window = SDL_CreateWindow("A Fun Time", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_SHOWN);
+	SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
+
+	SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
+
+	SDL_RenderClear(renderer);
+
+	SDL_RenderPresent(renderer);
+
+	SDL_Delay(10000000000);
+
+	return 0;
+}
